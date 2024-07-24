52482618ac62a3fa39eb88bbc2dd3aab90b166d5
DianaLuel <lueldiana@gmail.com>
2024-07-25 00:18:52 +0300

    Added files

---
+++ js/music-list.js
@@ -0,0 +1,146 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the /assets/tree/main/music/ folder."
+//   }
+
+let allMusic = [
+  {
+    name: "BLACKPINK-불장난-PLAYING-WITH-FIRE-M-V",
+    artist: "BLACKPINK",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "BLACKPINK-붐바야-BOOMBAYAH-M-V",
+    artist: "BLACKPINK",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "BLACKPINK-휘파람-WHISTLE-M-V",
+    artist: "BLACKPINK",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Honey, I m Good. - Andy Grammer",
+    artist: "Andy Grammer",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Johnny Orlando, DVBBS - I Don't",
+    artist: "Johnny Orlando, DVBBS",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Isabela Merced - PAPI",
+    artist: "Isabela Merced",
+    img: "music-6",
+    src: "music-6"
+  },
+  {
+    name: "Iamnotshane - Maybe My Soulmate Died",
+    artist: "Iamnotshane",
+    img: "music-7",
+    src: "music-7"
+  },
+  {
+    name: "Johnny Orlando - Waste My Time",
+    artist: "Johnny Orlando",
+    img: "music-8",
+    src: "music-8"
+  },
+  {
+    name: "Johnny Orlando - Vegas",
+    artist: "Johnny Orlando",
+    img: "music-9",
+    src: "music-9"
+  },
+  {
+    name: "Brittany Maggs - Killing My Vibe",
+    artist: "Brittany Maggs",
+    img: "music-10",
+    src: "music-10"
+  },
+  {
+    name: "Mabel - Don_t Call Me Up",
+    artist: "Mabel",
+    img: "music-11",
+    src: "music-11"
+  },
+  {
+    name: "Mabel - Selfish Love",
+    artist: "Mabel",
+    img: "music-12",
+    src: "music-12"
+  },
+  {
+    name: "The-Vamps-Just-My-Type",
+    artist: "The-Vamps",
+    img: "music-13",
+    src: "music-13"
+  },
+  {
+    name: "The-Vamps-Somebody-To-You-ft-Demi-Lovato.",
+    artist: "The-Vamps, Demi-Lovato",
+    img: "music-14",
+    src: "music-14"
+  },
+  {
+    name: "Trust-Fund-Baby-Why-Don-t-We",
+    artist: "Why-Don-t-We",
+    img: "music-15",
+    src: "music-15"
+  },
+  {
+    name: "8-Letters-Why-Don-t-We",
+    artist: "Why-Don-t-We",
+    img: "music-16",
+    src: "music-16"
+  },
+  {
+    name: "Daniel Skye_ Baby Ariel - Say It",
+    artist: "Daniel Skye_ Baby Ariel",
+    img: "music-17",
+    src: "music-17"
+  },
+  {
+    name: "Becky_G_Lovin_So_Hard",
+    artist: "Becky G",
+    img: "music-18",
+    src: "music-18"
+  },
+  {
+    name: "LANY_-_Super_Far ",
+    artist: "LANY",
+    img: "music-19",
+    src: "music-19"
+  },
+  {
+    name: "Hailee Steinfeld - Wrong Direction",
+    artist: "Hailee Steinfeld",
+    img: "music-20",
+    src: "music-20"
+  },
+  {
+    name: "Meghan-Trainor-Better-When-I-m-Dancin",
+    artist: "Meghan-Trainor",
+    img: "music-21",
+    src: "music-21"
+  },
+
+
+  // remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
